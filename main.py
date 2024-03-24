6db7946c572cffc0bec2d3948dbc4814c3cdc901
UentR <riggiquentin@gmail.com>
2024-03-24 18:33:53 +0100

    Refactor orbite_calculator function

--- main.py
+++ main.py
@@ -3,18 +3,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def orbite_calculator(*args):
-    N = len(args)
-    Dangle = []
-    for i in range(N//2):
-        Dangle.append(args[N//2+i]/args[i])
-    Coeff = [1, -1, 1]
+    N = len(args)//2
+    Dangle = np.zeros(N)
+    for i in range(N):
+        Dangle[i] = args[N+i]/args[i]
     def calculate_orbit_position(time):
-        Theta = [Dangle[i]*time for i in range(N//2)]
+        time = np.repeat(time, N, axis=0).reshape(N, time.shape[0])
+        Theta = time*Dangle[:, None]
+        COS = np.cos(Theta)
+        SIN = np.sin(Theta)
         x = 0
         y = 0
-        for i in range(N//2):
-            x += args[i]*math.cos(Theta[i])*Coeff[i]
-            y += args[i]*math.sin(Theta[i])*Coeff[i]
+        Coeff = 1
+        for i in range(N):
+            x += args[i]*COS[i]*Coeff
+            y += args[i]*SIN[i]*Coeff
+            Coeff *= -1
         return x, y
 
     return calculate_orbit_position
@@ -22,22 +26,21 @@ def orbite_calculator(*args):
 def main():
     R1 = 149
     R2 = 0.3
-    R3 = 228
-    V1 = 3
+    R3 = 228*3
+    V1 = 2.735*4
     V2 = 1.6
-    V3 = 2.4
-    lune_p2 = orbite_calculator(R2, R1, R3, V2, V1, V3)
-    terre = orbite_calculator(R1, V1)
+    V3 = 1.4
+    # lune_p2 = orbite_calculator(R2, R1, R3, V2, V1, V3)
+    terre_P1 = orbite_calculator(R1, R3, V1, V3)
     lune_p1 = orbite_calculator(R2, R1, V2, V1)
-    time = np.linspace(0, 13750, 100000)
-    Pos = [lune_p1(t) for t in time]
-    x, y = zip(*Pos)
-    plt.plot(x, y)
+    time = np.linspace(0, 2055*3, 100000)
+    x, y = terre_P1(time)
+    fig, ax = plt.subplots(figsize=(10, 10))
+    ax.plot(x, y)
     
     
-    plt.rcParams['axes.facecolor'] = 'white'
-    plt.rcParams['figure.facecolor'] = 'white'
-    plt.show()
+    fig.show()
+    fig.savefig("terre_p1.png")
 
 if __name__ == "__main__":
     main()
